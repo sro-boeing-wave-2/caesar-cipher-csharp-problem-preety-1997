86ef33c31871649fac27c4897b07bd441e00b621
Preety Kumari <preetydakshana@gmail.com>
2018-07-20 09:22:23 +0530

    solution updated

--- CaesarCipher/CaesarCipher.cs
+++ CaesarCipher/CaesarCipher.cs
@@ -5,9 +5,43 @@ namespace CaesarCipher
 {
     public static class RotationalCipher
     {
-        public static string Rotate(string text, int shiftKey)
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
-    }
+		public static string Rotate(string text, int shiftKey)
+		{
+			string Ciphertext = "";
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (((int)text[i] >=(int)'a' && (int)text[i] <= (int)'z'))
+				{
+					int sum = (int)text[i] + shiftKey;
+					if (sum >= 123)
+					{
+						Ciphertext += (char)('a'+(sum-123));
+					}
+					
+					else     
+					{
+						Ciphertext += (char)(text[i] + shiftKey);
+					}
+				}
+				else if (((int)text[i] >= 65 && (int)text[i] <= 90))
+				{
+					int sum = (int)text[i] + shiftKey;
+					if (sum >= 91)
+					{
+						Ciphertext += (char)('A' +(sum -91));
+					}
+					
+					else
+					{
+						Ciphertext += (char)(text[i] + shiftKey);
+					}
+				}
+				else {
+					Ciphertext += text[i];
+				}
+			}
+			return Ciphertext;
+
+		}
+	}
 }
